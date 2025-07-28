7841ec999b779d5192dcb0a425962d318b8829a6
Inshaullahpro <kundiinshaullah3@gmail.com>
2025-07-29 00:20:46 +0500

    Add files via upload

---
+++ 06-String.js
@@ -0,0 +1,28 @@
+const name  = "kundi";
+const repoCount = 56 ;
+
+console.log(name + repoCount + " value")
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('inshaullah');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(8, 3)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 5);
+console.log(anotherString);
+
+const newStringOne  = '  inshaullah  ';
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://inshaullah.com/insha20%kundi";
+console.log(url.replace('20% ', '-'));
+console.log(url.includes('insha'));
+console.log(gameName.split('-'));
